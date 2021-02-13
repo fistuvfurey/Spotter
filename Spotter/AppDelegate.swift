66c901ac004f8832eac3cb209c0cbd1fe7c24036
Aidan Furey <aidan.t.furey@gmail.com>
2021-02-13 15:26:27 -0500

    Connect to Parse backend. User can create an account and log in.

--- Spotter/AppDelegate.swift
+++ Spotter/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "qvDnSnntUdvCTRf6UrP4XlJJ5z4o5WAGuU71kzpj"
+                    $0.clientKey = "HQ6fkZUyOjrOuIVlzGrhbPoBmbsJ4yUtt7395UOS"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
